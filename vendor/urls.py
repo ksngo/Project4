a5e144e054f1e1468447272175844b4774404443
ksngo <sevenelectrons@gmail.com>
2020-05-20 02:52:29 +0000

    able to delete vendor_town row from vendor_town table. Same for vendor_postal.

--- vendor/urls.py
+++ vendor/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('profile/', vendor.views.view_vendor_profile, name='view_vendor_profile_route'),
     path('profile/<vendor_profile_id>', vendor.views.edit_vendor_profile, name='edit_vendor_profile_route'),
     path('profile/delete/<vendor_profile_id>', vendor.views.delete_vendor_profile, name='delete_vendor_profile_route'),
-    path('profile/<vendor_profile_id>/delivery_area', vendor.views.create_delivery_area, name='create_delivery_area_route')
-
+    path('profile/<vendor_profile_id>/delivery_area', vendor.views.create_delivery_area, name='create_delivery_area_route'),
+    path('profile/<vendor_profile_id>/delivery_area/remove_town_vendor/<town_vendor_id>', vendor.views.remove_town_vendor, name='remove_town_vendor_route'),
+    path('profile/<vendor_profile_id>/delivery_area/remove_postal_vendor/<postal_vendor_id>', vendor.views.remove_postal_vendor, name='remove_postal_vendor_route')
 ]
